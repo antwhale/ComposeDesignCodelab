39de057338b0aa3792828732bea1af70e46a604d
Antwhale <olollolo339@naver.com>
2024-03-26 18:52:55 +0900

    9, Home section - Slot APIs

--- app/src/main/java/io/github/antwhale/compose/MainActivity.kt
+++ app/src/main/java/io/github/antwhale/compose/MainActivity.kt
@@ -8,15 +8,23 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.animation.expandHorizontally
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -35,8 +43,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.drawable.toDrawable
 import io.github.antwhale.compose.ui.theme.ComposeDesignCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -62,6 +72,34 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun HomeSection(
+    @StringRes title: Int,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+){
+    Column(modifier){
+        Text(
+            stringResource(title),
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier
+                .paddingFromBaseline(top = 40.dp, bottom = 16.dp)
+                .padding(horizontal = 16.dp)
+            )
+        content()
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE, apiLevel = 33)
+@Composable
+fun HomeSectionPreview() {
+    ComposeDesignCodelabTheme() {
+        HomeSection(R.string.align_your_body) {
+            AlignYourBodyRow()
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -92,6 +130,21 @@ fun SearchBar(
 }
 
 @Composable
+fun AlignYourBodyRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(alignYourBodyData) { item ->
+            AlignYourBodyElement(drawable = item.drawable, text = item.text)
+        }
+    }
+}
+
+@Composable
 fun AlignYourBodyElement(
     @DrawableRes drawable: Int,
     @StringRes text: Int,
@@ -102,7 +155,7 @@ fun AlignYourBodyElement(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            painter = painterResource(id = R.drawable.volleyball),
+            painter = painterResource(id = drawable),
             contentDescription = null,
             contentScale = ContentScale.Crop,
             modifier = Modifier
@@ -110,7 +163,7 @@ fun AlignYourBodyElement(
                 .clip(CircleShape)
         )
         Text(
-            text = "AlignYourBodyElement",
+            text = stringResource(text),
             modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp),
             style = MaterialTheme.typography.bodyMedium
         )
@@ -120,13 +173,13 @@ fun AlignYourBodyElement(
 @Composable
 fun FavoriteCollectionCard(
     @DrawableRes drawable: Int,
-    text: String,
+    @StringRes text: Int,
     modifier: Modifier = Modifier
 ) {
     Surface(
         shape = MaterialTheme.shapes.medium,
         color = MaterialTheme.colorScheme.surfaceVariant,
-        modifier = modifier.width(255.dp)
+        modifier = modifier.width(255.dp),
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
@@ -139,7 +192,7 @@ fun FavoriteCollectionCard(
                 modifier = Modifier.size(80.dp)
             )
             Text(
-                text = text,
+                text = stringResource(id = text),
                 style = MaterialTheme.typography.titleMedium,
                 modifier = Modifier.padding(horizontal = 16.dp)
                 )
@@ -147,16 +200,48 @@ fun FavoriteCollectionCard(
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun FavoriteCollectionCardPreview(){
-    FavoriteCollectionCard(
-        text = "FavoriteCollectionCard",
-        drawable = R.drawable.volleyball,
-        modifier = Modifier.padding(8.dp)
-    )
+fun FavoriteCollectionsGrid(
+    modifier: Modifier = Modifier
+) {
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(2),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier.height(168.dp)
+    ) {
+        items(favoriteCollectionsData) { item ->
+            FavoriteCollectionCard(item.drawable, item.text, Modifier.height(80.dp))
+        }
+    }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun FavoriteCollectionsGridPreview() {
+    FavoriteCollectionsGrid()
+}
+
+
+//@Preview(showBackground = true)
+//@Composable
+//fun FavoriteCollectionCardPreview(){
+//    FavoriteCollectionCard(
+//        text = "FavoriteCollectionCard",
+//        drawable = R.drawable.volleyball,
+//        modifier = Modifier.padding(8.dp)
+//    )
+//}
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun AlignYourBodyRowPreview(){
+    AlignYourBodyRow()
+}
 
 @Preview(showBackground = true)
 @Composable
@@ -173,3 +258,25 @@ fun GreetingPreview() {
     }
 }
 
+private val alignYourBodyData = listOf(
+    R.drawable.ab1_inversions to R.string.ab1_inversions,
+    R.drawable.ab2_quick_yoga to R.string.ab2_quick_yoga,
+    R.drawable.ab3_stretching to R.string.ab3_stretching,
+    R.drawable.ab4_tabata to R.string.ab4_tabata,
+    R.drawable.ab5_hiit to R.string.ab5_hiit,
+    R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga
+).map { DrawableStringPair(it.first, it.second) }
+
+private val favoriteCollectionsData = listOf(
+    R.drawable.fc1_short_mantras to R.string.fc1_short_mantras,
+    R.drawable.fc2_nature_meditations to R.string.fc2_nature_meditations,
+    R.drawable.fc3_stress_and_anxiety to R.string.fc3_stress_and_anxiety,
+    R.drawable.fc4_self_massage to R.string.fc4_self_massage,
+    R.drawable.fc5_overwhelmed to R.string.fc5_overwhelmed,
+    R.drawable.fc6_nightly_wind_down to R.string.fc6_nightly_wind_down
+).map { DrawableStringPair(it.first, it.second) }
+
+private data class DrawableStringPair(
+    @DrawableRes val drawable: Int,
+    @StringRes val text: Int
+)

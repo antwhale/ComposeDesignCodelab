75850c34cef0cc4fe81051eb69da8a9f46c147a5
Antwhale <olollolo339@naver.com>
2024-03-28 13:51:24 +0900

    Finish

--- app/src/main/java/io/github/antwhale/compose/MainActivity.kt
+++ app/src/main/java/io/github/antwhale/compose/MainActivity.kt
@@ -1,17 +1,22 @@
 package io.github.antwhale.compose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.text.Layout
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.animation.expandHorizontally
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -25,17 +30,31 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
@@ -47,21 +66,16 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.drawable.toDrawable
+import io.github.antwhale.compose.ui.theme.AppTheme
 import io.github.antwhale.compose.ui.theme.ComposeDesignCodelabTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeDesignCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            val windowSizeClass = calculateWindowSizeClass(this)
+            MySootheApp(windowSizeClass)
         }
     }
 }
@@ -73,6 +87,165 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+
+@Composable
+fun MySootheApp(windowSize: WindowSizeClass) {
+    val TAG = "MySootheApp"
+    when(windowSize.widthSizeClass) {
+        WindowWidthSizeClass.Compact -> {
+            Log.d(TAG, "WindowWidthSizeClass.Compact")
+            MySootheAppPortrait()
+        }
+        WindowWidthSizeClass.Medium -> {
+            Log.d(TAG, "WindowWidthSizeClass.Medium")
+            MySootheAppLandscape()
+        }
+        WindowWidthSizeClass.Expanded -> {
+            Log.d(TAG, "WindowWidthSizeClass.Expanded")
+            MySootheAppLandscape()
+        }
+    }
+}
+
+@Composable
+fun MySootheAppLandscape() {
+    /*ComposeDesignCodelabTheme*/AppTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            Row() {
+                SootheNavigationRail()
+                HomeScreen()
+            }
+        }
+    }
+}
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    name = "DefaultPreviewLight"
+)
+@Preview(showBackground = true, apiLevel = 33)
+@Composable
+fun MySootheAppLandscapePreview() {
+    MySootheAppLandscape()
+}
+
+@Composable
+private fun SootheNavigationRail(modifier: Modifier = Modifier) {
+    NavigationRail(
+        modifier = modifier.padding(start = 8.dp, end = 8.dp),
+        containerColor = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            NavigationRailItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.Home,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text(stringResource(id = R.string.bottom_navigation_home))
+                },
+                selected = true,
+                onClick = {}
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            NavigationRailItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.AccountCircle,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text(stringResource(id = R.string.bottom_navigation_profile))
+                },
+                selected = true,
+                onClick = {}
+            )
+        }
+    }
+}
+
+@Composable
+fun MySootheAppPortrait() {
+    /*ComposeDesignCodelabTheme*/AppTheme {
+        Scaffold (
+            bottomBar = { SootheBottomNavigation() }
+        ) { padding ->
+            HomeScreen(Modifier.padding(padding))
+        }
+    }
+}
+
+@Composable
+private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        modifier = modifier
+    ) {
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(R.string.bottom_navigation_home)
+                )
+            },
+            selected = true,
+            onClick = {  }
+        )
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(R.string.bottom_navigation_profile)
+                )
+            },
+            selected = false,
+            onClick = {  }
+        )
+    }
+}
+
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier
+            .verticalScroll(rememberScrollState())
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        SearchBar(Modifier.padding(horizontal = 16.dp))
+        HomeSection(title = R.string.align_your_body) {
+            AlignYourBodyRow()
+        }
+        HomeSection(title = R.string.favorite_collections) {
+            FavoriteCollectionsGrid()
+        }
+        Spacer(Modifier.height(16.dp))
+    }
+}
+
+
 
 @Composable
 fun HomeSection(
@@ -89,16 +262,6 @@ fun HomeSection(
                 .padding(horizontal = 16.dp)
             )
         content()
-    }
-}
-
-@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE, apiLevel = 33)
-@Composable
-fun HomeSectionPreview() {
-    ComposeDesignCodelabTheme() {
-        HomeSection(R.string.align_your_body) {
-            AlignYourBodyRow()
-        }
     }
 }
 
@@ -151,8 +314,9 @@ fun AlignYourBodyElement(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier,
+        modifier = modifier.background(MaterialTheme.colorScheme.surfaceVariant),
         horizontalAlignment = Alignment.CenterHorizontally
+
     ) {
         Image(
             painter = painterResource(id = drawable),
@@ -214,6 +378,40 @@ fun FavoriteCollectionsGrid(
         items(favoriteCollectionsData) { item ->
             FavoriteCollectionCard(item.drawable, item.text, Modifier.height(80.dp))
         }
+    }
+}
+
+@Preview(showBackground = true, apiLevel = 33)
+@Composable
+fun MySootheAppPortraitPreview() {
+    AppTheme {
+        MySootheAppPortrait()
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE, apiLevel = 33)
+@Composable
+fun HomeSectionPreview() {
+    ComposeDesignCodelabTheme() {
+        HomeSection(R.string.align_your_body) {
+            AlignYourBodyRow()
+        }
+    }
+}
+
+@Preview(showBackground = true, apiLevel = 33)
+@Composable
+fun SootheBottomNavigationPreview(){
+    ComposeDesignCodelabTheme {
+        SootheBottomNavigation()
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE, heightDp = 180, apiLevel = 33)
+@Composable
+fun ScreenContentPreview() {
+    ComposeDesignCodelabTheme {
+        HomeScreen()
     }
 }
 
